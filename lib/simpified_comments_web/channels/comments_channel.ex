c8ae74cd563b8ce794e1868853690ade3d6a5704
Chris Nelson <superchrisnelson@gmail.com>
2023-10-02 16:30:28 -0400

    real time comments

--- lib/simpified_comments_web/channels/comments_channel.ex
+++ lib/simpified_comments_web/channels/comments_channel.ex
@@ -2,14 +2,22 @@ defmodule SimpifiedCommentsWeb.CommentsChannel do
   @moduledoc false
 
   use LiveState.Channel, web_module: SimpifiedCommentsWeb
+  alias Phoenix.PubSub
 
   @impl true
   def init(_channel, _params, _socket) do
+    PubSub.subscribe(SimpifiedComments.PubSub, "comments")
     {:ok, %{comments: []}}
   end
 
   @impl true
-  def handle_event("add-comment", comment, %{comments: comments} = state) do
+  def handle_event("add-comment", comment, state) do
+    PubSub.broadcast(SimpifiedComments.PubSub, "comments", {:add_comment, comment})
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_message({:add_comment, comment}, %{comments: comments} = state) do
     {:noreply, Map.put(state, :comments, [comment | comments])}
   end
 
